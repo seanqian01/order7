dba7c3bab37c509610575c92b37808e71884a3c0
seanqian <314792726@qq.com>
2024-01-20 15:51:03 +0800

    model

--- alert/models.py
+++ alert/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-
 # Create your models here.
 # from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class stra_Alert(models.Model):
@@ -49,3 +49,37 @@ class stra_Order(models.Model):
         db_table = 'stra_order'
         verbose_name = '交易订单'
         verbose_name_plural = verbose_name
+#
+#
+# class Strategy(models.Model):
+#     name = models.CharField(max_length=120, unique=True, verbose_name="策略名称")
+#
+#     TIME_CYCLE_CHOICES = [
+#         ('1m', '1分钟'),
+#         ('5m', '5分钟'),
+#         ('15m', '15分钟'),
+#         ('30m', '30分钟'),
+#         ('1h', '1小时'),
+#         ('2h', '2小时'),
+#         ('4h', '4小时'),
+#         ('6h', '6小时'),
+#         ('12h', '12小时'),
+#         ('1d', '1天'),
+#         ('1w', '1周'),
+#     ]
+#     strategy_time_cycle = models.CharField(choices=TIME_CYCLE_CHOICES, max_length=50, verbose_name="策略应用时间周期")
+#     strategy_desc = models.TextField(max_length=255, blank=True, verbose_name="策略描述")
+#
+#     status = models.BooleanField(default=True, verbose_name="策略状态")
+#     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+#     update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+#     stra_creater = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="策略创建者")
+#
+#     class Meta:
+#         db_table = 'strategy'
+#         verbose_name = '交易策略'
+#         verbose_name_plural = verbose_name
+#         ordering = ('-update_time',)
+#
+#     def __str__(self):
+#         return self.name
