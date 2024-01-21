a7e9208cc09dbc8954f134bd51a3a52ca8c1da0e
seanqian <314792726@qq.com>
2024-01-21 17:13:38 +0800

    serlizer init

---
+++ alert/serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from .models import Strategy
+from django.contrib.auth.models import User
+
+
+class StrategySerializer(serializers.ModelSerializer):
+    stra_creater=serializers.ReadOnlyField(source='stra_creater.username')
+
+    class Meta:
+        model = Strategy
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # fields = ['id', 'username', 'strategies']
+        fields = '__all__'
